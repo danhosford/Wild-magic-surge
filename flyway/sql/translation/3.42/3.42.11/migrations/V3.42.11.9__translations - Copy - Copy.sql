a1607493f7440e4f92d2b6aa0c6740b6cf85fae8
danhosford <98965791+danhosford@users.noreply.github.com>
2024-08-16 11:26:29 +0100

    lake

---
+++ flyway/sql/translation/3.42/3.42.11/migrations/V3.42.11.9__translations - Copy - Copy.sql
@@ -0,0 +1,76 @@
+IF OBJECT_ID('tempdb..#translations') IS NOT NULL DROP TABLE #translations;
+GO
+
+CREATE TABLE #translations
+(
+    [id]      INT,
+    [active]  BIT,
+    [uuid]    VARCHAR(50) NOT NULL,
+    [section] VARCHAR(255),
+    [page]    VARCHAR(255),
+    [target]  VARCHAR(525)
+);
+
+INSERT INTO #translations ([id], [active], [uuid], [section], [page], [target])
+VALUES (2387, 1, '6f5c6fdc-7322-4cdc-a178-8a5d3a258cf3', '', '', N'Gas Monitoring entry deleted successfully.'),
+       (2388, 1, 'a28e7e05-4c1e-4a26-85f4-65e98a4b3aa4', '', '', N'Measured By')
+;
+
+SET IDENTITY_INSERT [kioskLanguage] ON;
+
+BEGIN
+
+    BEGIN TRANSACTION;
+
+    INSERT INTO [kioskLanguage] ( [klangID], [klangIsActive]
+                                , [klangUUID], [kbcSection], [kbcPage]
+                                , [en], [en_ie]
+                                , [klangEnglish], [en_GB])
+    SELECT [translation].[id]
+         , [translation].[active]
+         , [translation].[uuid]
+         , [translation].[section]
+         , [translation].[page]
+         , REPLACE([translation].[target], '''', '&#x27;')
+         , REPLACE([translation].[target], '''', '&#x27;')
+         , REPLACE([translation].[target], '''', '&#x27;')
+         , REPLACE([translation].[target], '''', '&#x27;')
+    FROM #translations AS [translation]
+             LEFT JOIN [kioskLanguage] AS [kiosk] ON [kiosk].[klangID] = [translation].[id]
+    WHERE [kiosk].[klangID] IS NULL;
+
+    COMMIT TRANSACTION;
+END
+
+SET IDENTITY_INSERT [kioskLanguage] OFF;
+
+BEGIN
+
+    BEGIN TRANSACTION;
+
+    UPDATE [kiosk]
+    SET [kiosk].[en]           = REPLACE(
+            REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'), '&#x3a;',
+            ':')
+      , [kiosk].[en_IE]        = REPLACE(
+            REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'), '&#x3a;',
+            ':')
+      , [kiosk].[klangEnglish] = REPLACE(
+            REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'), '&#x3a;',
+            ':')
+      , [kiosk].[en_GB]        = REPLACE(
+            REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'), '&#x3a;',
+            ':')
+    FROM [kioskLanguage] AS [kiosk]
+             INNER JOIN #translations AS [translation] ON [translation].[id] = [kiosk].[klangID]
+    WHERE REPLACE(REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'),
+                  '&#x3a;', ':') != ISNULL([kiosk].[en], '') COLLATE Latin1_General_CI_AS
+       OR REPLACE(REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'),
+                  '&#x3a;', ':') != ISNULL([kiosk].[en_IE], '') COLLATE Latin1_General_CI_AS
+       OR REPLACE(REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'),
+                  '&#x3a;', ':') != ISNULL([kiosk].[klangEnglish], '') COLLATE Latin1_General_CI_AS
+       OR REPLACE(REPLACE(REPLACE(REPLACE([translation].[target], '''', '&#x27;'), '(', '&#x28;'), ')', '&#x29;'),
+                  '&#x3a;', ':') != ISNULL([kiosk].[en_GB], '') COLLATE Latin1_General_CI_AS;
+
+    COMMIT TRANSACTION;
+END
